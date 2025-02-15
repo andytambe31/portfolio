75c61141fe7e305f1c8bacc43eddfbbb07968c52
andytambe31 <tambe.aniruddha3110@gmail.com>
2025-02-15 12:33:03 -0500

    feat: navbar completed

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const toggleMenu = () => {
+    const offcanvasContainer = document.getElementsByClassName("offcanvas")[0];
+    if (offcanvasContainer) {
+        offcanvasContainer.classList.toggle('show');
+    }
+ }
+ 
+ document.addEventListener('DOMContentLoaded', () => {
+     const hamburgerBtn = document.getElementById('hamburger-btn');
+     const offcanvasContainer = document.getElementsByClassName("offcanvas")[0];
+ 
+     hamburgerBtn.addEventListener('click', (e) => {
+         e.stopPropagation(); // Prevent click from propagating to document
+         toggleMenu();
+     });
+ 
+     document.addEventListener('click', (e) => {
+         if (offcanvasContainer.classList.contains('show') && !offcanvasContainer.contains(e.target)) {
+             toggleMenu();
+         }
+     });
+ });
+ 
